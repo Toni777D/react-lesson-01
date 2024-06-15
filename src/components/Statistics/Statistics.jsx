66130f4cad1d38a0af5c29835debd57c548c7d81
r1shek <rostic523@gmail.com>
2024-06-15 15:31:40 +0200

    task 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,26 @@
-// import { FaRegThumbsUp } from 'react-icons/fa';
-// import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
-// import { GiTreeDoor } from 'react-icons/gi';
-// import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiTreeDoor } from 'react-icons/gi';
+import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
+import style from './Statistics.module.css';
 
-export const Statistics = () => {
-  return <h2>Statistics</h2>;
+export const Statistics = ({ title, stats }) => {
+  const icons = [
+    <FaRegThumbsUp key="0" />,
+    <MdPeople key="1" />,
+    <MdOutlineProductionQuantityLimits key="2" />,
+    <GiTreeDoor key="3" />,
+  ];
+
+  return (
+    <>
+      {title && <h2 className={style.title}>{title}</h2>}
+      <ul className={style.list}>
+        {stats.map((stat, index) => (
+          <StatisticsItem key={stat.id} date={stat} icon={icons[index]} />
+        ))}
+      </ul>
+      ;
+    </>
+  );
 };
--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,3 +1,15 @@
-export const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import styled from './Statistics.module.css';
+import { IconContext } from 'react-icons';
+
+export const StatisticsItem = ({ date, icon }) => {
+  console.log(icon);
+
+  return (
+    <li className={styled.item}>
+      <IconContext.Provider value={{ size: 60 }}>{icon}</IconContext.Provider>
+
+      <span className={styled.counter}>{date.total}</span>
+      <p className={styled.text}>{date.title}</p>
+    </li>
+  );
 };
