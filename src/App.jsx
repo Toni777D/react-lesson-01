2a2352efc31e929ef85920de1ba9b3d8b48016ef
kholshevnikova <151967304+kholshevnikova@users.noreply.github.com>
2024-06-15 17:03:27 +0300

    task-3

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,8 @@ import {
 
 import stats from './data/stats.json';
 
+import forbes from 'data/forbes.json';
+
 export const App = () => {
   return (
     <Section>
@@ -50,7 +52,7 @@ export const App = () => {
         <Statistics title="Main Statistics" stats={stats} />
 
         <Heading title="Task 3 Forbes list" top bottom />
-        <ForbesList />
+        <ForbesList list={forbes}/>
 
         <Heading title="Task 4 Crypto history" top bottom />
         <CryptoHistory />
